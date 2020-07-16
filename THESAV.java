75ab307b0a7175fc593846d0b3b500e3d2db8017
Swaraj <swaraj.ravi@publicissapient.com>
2020-07-17 01:36:32 +0530

    updating file with problem link

--- THESAV.java
+++ THESAV.java
@@ -1,4 +1,6 @@
-/* package codechef; // don't place package name! */
+/* 
+Problem Link- https://www.codechef.com/problems/THESAV
+package codechef; // don't place package name! */
 
 import java.util.*;
 import java.lang.*;
@@ -7,38 +9,34 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-    public static String wasAbleToDistributeFood(String inp){
-        if(inp.length()>0 && inp.charAt(0)=='0')
+    public static String canFeedAllHouses(String inp){
+        if(inp.charAt(0)=='0')
             return "NO";
-        int count= 0, i;
-        for(i=0; i< inp.length(); i++){
-            switch(inp.charAt(i)){
-                case '*': 
-                    count= 2;
-                    break;
-                case '0':
-                    if(count== 0)
+        else{
+            int counter= 2;
+            for(int i=1; i<inp.length(); i++){
+                if(inp.charAt(i)=='*')
+                    counter= 2;
+                else{
+                    if(counter==0)
                         return "NO";
-                    count--;
+                    else
+                        counter--;
+                }
             }
+            return "YES";
         }
-        return "YES";
     }
-	public static void main (String[] args) throws java.lang.Exception
+    public static void main (String[] args) throws java.lang.Exception
 	{
-	    int t;
-	    String currInp;
 	    Scanner sc= new Scanner(System.in);
-	    try{
-	        if(sc.hasNext()){
-	            t= sc.nextInt();
-	            while(t--> 0 && sc.hasNext()){
-	                currInp= sc.next();
-	                System.out.println(wasAbleToDistributeFood(currInp));
-	            }
+	    if(sc.hasNext()){
+	        int t= sc.nextInt();
+	        while(t-->0 && sc.hasNext()){
+	            String inp= sc.next();
+	            System.out.println(canFeedAllHouses(inp));
+	            
 	        }
-	    }catch(Exception e){
-	        System.out.println("Some exception occure "+ e);
 	    }
 	}
 }
